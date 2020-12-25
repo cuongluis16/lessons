e8fcbfd6e05d1d240318297f898af977806548b6
cuongluis16 <75594110+cuongluis16@users.noreply.github.com>
2020-12-25 10:28:24 +0900

    JavaFX

--- JavaFX/Test.java
+++ JavaFX/Test.java
@@ -1,9 +1,12 @@
+import javafx.animation.Animation;
+import javafx.animation.ScaleTransition;
+import javafx.util.Duration;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.Group;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 
 public class Test extends Application {
   public static void main( String[] args ) {
@@ -11,16 +14,26 @@ public class Test extends Application {
   }
   @Override
   public void start( Stage stage ) throws Exception {
-    Group root = new Group();
-    Scene scene = new Scene( root, 500, 400 );
-
-    Circle circle = new Circle( 250, 200, 80 );
-
-    circle.setFill( Color.BLUE );
-
-    root.getChildren().add( circle );
-
+    View v = new View();
+    Scene scene = new Scene( v, 400, 300 );
     stage.setScene( scene );
     stage.show();
+  }
+}
+
+class View extends Group {
+  public View() {
+    Rectangle rect = new Rectangle( 0, 0, 50, 40 );
+    rect.setFill( Color.CYAN );
+    ScaleTransition animation = new ScaleTransition( Duration.seconds( 2 ), rect );
+    animation.setFromX( 1 );
+    animation.setFromY( 1 );
+    animation.setToX( 5 );
+    animation.setToY( 5 );
+    animation.setCycleCount( Animation.INDEFINITE );
+    animation.setAutoReverse( true );
+
+    animation.play();
+    getChildren().add( rect );
   }
 }
